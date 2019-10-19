76eb7feb5843c29389627c0434c25d7d0b2ca1a7
Larry Tseng <larry.tseng.1@gmail.com>
2019-10-19 00:12:18 -0400

    Generate initial files

---
+++ FitnessApp/FitnessApp WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  FitnessApp WatchKit Extension
+//
+//  Created by Larry Tseng on 10/19/19.
+//  Copyright © 2019 FitnessApp. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ FitnessApp/FitnessApp WatchKit Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  FitnessApp WatchKit Extension
+//
+//  Created by Larry Tseng on 10/19/19.
+//  Copyright © 2019 FitnessApp. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
