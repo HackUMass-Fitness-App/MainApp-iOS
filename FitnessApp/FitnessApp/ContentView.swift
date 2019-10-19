52f9fe463c3afaf972b586c6840ca8c452166687
Emmie Ohnuki <emaohn@gmail.com>
2019-10-19 12:14:21 -0400

    Added navigation bar and button

--- FitnessApp/FitnessApp/ContentView.swift
+++ FitnessApp/FitnessApp/ContentView.swift
@@ -9,12 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            //Text("Hello World")
+            Button(action: {
+                print("select workout")
+            }) {
+                Text("Select Workout")
+            }
+        
+            .navigationBarTitle("Hi, Anthony.")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
         ContentView()
     }
